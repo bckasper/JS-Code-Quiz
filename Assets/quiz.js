51a1b5de173f3f342a730a58dfaed284b78db3fe
Ben Kasper <bckasper@gmail.com>
2021-08-15 17:14:38 -0400

    made a few font and display changes on the CSS; changed the StartQuiz function to REMOVE the uneeded elements and began the function of showing the first question

--- Assets/quiz.js
+++ Assets/quiz.js
@@ -3,69 +3,82 @@ var header = document.getElementById("header")
 var timerElement = document.getElementById("timer")
 var welcomeText = document.getElementById("welcome-text")
 var startButton = document.getElementById("startBtn")
+var question = document.getElementById("question")
+var answersUL = document.getElementById("answers")
+
 
 
 // Questions
 var questions = [
     {
-        question: "How many variable types are there?",
+        question: "This is question number 1?",
         choices: [1,2,3,4],
         answer: 1,
     },
     {
-        question: "How many variable types are there?",
+        question: "This is question number 2",
         choices: [1,2,3,4],
         answer: 1,
     },
     {
-        question: "How many variable types are there?",
+        question: "This is question number 3",
         choices: [1,2,3,4],
         answer: 1,
     },
     {
-        question: "How many variable types are there?",
+        question: "This is question number 4",
         choices: [1,2,3,4],
         answer: 1,
     },
     {
-        question: "How many variable types are there?",
+        question: "This is question number 5",
         choices: [1,2,3,4],
         answer: 1,
     },
     {
-        question: "How many variable types are there?",
+        question: "This is question number 6",
         choices: [1,2,3,4],
         answer: 1,
     },
     {
-        question: "How many variable types are there?",
+        question: "This is question number 7",
         choices: [1,2,3,4],
         answer: 1,
     },
     {
-        question: "How many variable types are there?",
+        question: "This is question number 8",
         choices: [1,2,3,4],
         answer: 1,
     },
     {
-        question: "How many variable types are there?",
+        question: "This is question number 9",
         choices: [1,2,3,4],
         answer: 1,
     },
     {
-        question: "How many variable types are there?",
+        question: "This is question number 10",
         choices: [1,2,3,4],
         answer: 1,
     },
 ]
 
 
+// Functions of the application
 
+//Start quiz function will remove the header text, welcome text, button, and render the first question
 function startQuiz(){
-    header.innerHTML = ""
-    welcomeText.innerHTML = ""
-    startButton.setAttribute("style", "display:none")
-    startButton.disabled
+    header.remove()
+    welcomeText.remove()
+    startButton.remove()
+
+    showquestion()
 }
 
+function showquestion(){
+    
+}
+
+
+// My event listeners
 startButton.addEventListener("click", startQuiz)
+

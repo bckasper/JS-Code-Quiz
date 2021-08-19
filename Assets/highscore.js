38a5bbe8ea50b86e18965b721a3734fd070fac87
Ben Kasper <bckasper@gmail.com>
2021-08-18 20:48:07 -0400

    finished the appending HighScores function and clear high scores function;

--- Assets/highscore.js
+++ Assets/highscore.js
@@ -1,11 +1,37 @@
 //element variables
 var clearHighScoresBtn = document.getElementById("clearHighScore")
+var scoreOL = document.getElementById("highscores")
+var highScoreHeader = document.getElementById("header")
+var highScoreContainer = document.getElementById("container")
 
+highScoreHeader.setAttribute("style","margin:3rem auto 2rem auto;")
+highScoreContainer.setAttribute("style","min-height:150%;")
+scoreOL.setAttribute("style","margin-bottom: 2rem")
+clearHighScoresBtn.setAttribute("style","margin-bottom: 2rem")
+
+// Appends a UL to the highscores page and appends a LI for each score in the local storage
+function appendHighScores(){
+
+    var allScores = JSON.parse(localStorage.getItem("allScores"))
+    
+
+    for(let i=0; i<allScores.length; i++){
+        var scoreLi = document.createElement("li");
+        scoreLi.textContent = allScores[i].userName+": "+allScores[i].userScore;
+        scoreOL.appendChild(scoreLi);
+    }
+
+}
+
+appendHighScores()
 
 
 // clear local storage
 function clearLocalStorage(){
     localStorage.clear();
+    for(i=0; i<scoreOL.children.length; i++){
+        scoreOL.children[i].remove()
+    }
 }
 
 
